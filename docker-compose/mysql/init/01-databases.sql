10da1696af1903f14008033a2a35a4580fdd335f
abdulkareemsasah <abdulkareemsasah@gmail.com>
2024-05-26 12:28:09 +0300

    zasdsadsssas

--- docker-compose/mysql/init/01-databases.sql
+++
@@ -1,7 +0,0 @@
-# create databases
-CREATE DATABASE IF NOT EXISTS `univercity_system`;
-
-# create local_developer user and grant rights
-CREATE USER 'admin'@'db' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
-
